13e130851426ad7a07e24913791b4a505c49dda0
Rahul Patel <rp521@kent.ac.uk>
2022-10-24 12:06:31 +0100

    added service layer

--- NorthwindAPI/NorthwindAPI/Program.cs
+++ NorthwindAPI/NorthwindAPI/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using NorthwindAPI.Models;
+using NorthwindAPI.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,14 +10,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<NorthwindContext>(opt => opt.UseSqlServer(builder.Configuration["default"]));
+builder.Services.AddDbContext<NorthwindContext>(opt => opt.UseInMemoryDatabase("NorthwindMemory"));
+builder.Services.AddScoped<IProductService, ProductService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();   
+    //app.UseSwagger();
+    //app.UseSwaggerUI(); 
+
 }
 
 app.UseHttpsRedirection();
---
+++ NorthwindAPI/NorthwindAPI/Services/ProductService.cs
@@ -0,0 +1,58 @@
+﻿using NorthwindAPI.Models;
+
+namespace NorthwindAPI.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly NorthwindContext _context;
+      
+        public ProductService(NorthwindContext context)
+        {
+            _context = context;
+        }
+        public Task AddProductAsync(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetAllProductsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetProductByCategoryIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetProductByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetProductByNameAsync(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetProductBySupplierIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ProductsExsits(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveProductAsync(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> SaveChangesAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
