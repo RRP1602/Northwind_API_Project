87007c45ef39e92034458dc878a709c973fe4fbe
n4tn <109428583+n4tn@users.noreply.github.com>
2022-10-24 14:20:30 +0100

    Fixed project dependancies

--- NorthwindAPI/NorthwindAPI_Tests/Service Layer Tests.cs
+++ NorthwindAPI/NorthwindAPI_Tests/Service Layer Tests.cs
@@ -1,15 +1,17 @@
 ﻿using NorthwindAPI.Models;
 using NorthwindAPI.Services;
 using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
 
 namespace NorthwindAPI_Tests
 
 {
-    public class Tests
+    public class ServiceTests
     {
         private NorthwindContext _context;
         private IProductService _sut;
-
+        
+        [SetUp]
         public void OneTimeSetup()
         {
             var options = new DbContextOptionsBuilder<NorthwindContext>()
@@ -24,15 +26,15 @@ namespace NorthwindAPI_Tests
         public void GivenProducts_GetAllProductsAsync_ReturnsAllProducts()
         {
             var result = _sut.GetAllProductsAsync();
-            Assert.That(result.Value(), Is.TypeOf<List<Product>>());
+            Assert.That(result, Is.TypeOf<List<Product>>());
         }
 
         [Test]
-        public void GivenProduct_ AddProductAsync_ReturnsNewProduct()
+        public void GivenProduct_AddProductAsync_ReturnsNewProduct()
         {
 
-            var result = _sut.AddProductAsync(new Product { });
-            Assert.That(result.UnitPrice, Is.EqualTo(10))
+            var result = _sut.AddProductAsync(new Product { ProductId = 10, ProductName = "Freddo", SupplierId = 10, CategoryId = 10, QuantityPerUnit = "132", UnitPrice = 10, UnitsInStock = 2, UnitsOnOrder = 1, ReorderLevel = 5 });
+            Assert.That(result, Is.EqualTo(1.50));
         }
 
     }
