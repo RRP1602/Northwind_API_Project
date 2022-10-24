e34a98af39e1e8293485a04b68e0d9c657fa54dc
n4tn <109428583+n4tn@users.noreply.github.com>
2022-10-24 14:02:16 +0100

    Service layer tests init

---
+++ NorthwindAPI/NorthwindAPI_Tests/Service Layer Tests.cs
@@ -0,0 +1,39 @@
+﻿using NorthwindAPI.Models;
+using NorthwindAPI.Services;
+using System.Collections.Generic;
+
+namespace NorthwindAPI_Tests
+
+{
+    public class Tests
+    {
+        private NorthwindContext _context;
+        private IProductService _sut;
+
+        public void OneTimeSetup()
+        {
+            var options = new DbContextOptionsBuilder<NorthwindContext>()
+                .UseInMemoryDatabase(databaseName: "NorthwindDB").Options;
+            _context = new NorthwindContext(options);
+            _sut = new ProductService(_context);
+           //  _sut.AddProductAsync(new Product {ProductID = 1, ProductName = "Freddo",SupplierID = 1,CategoryID = 1, QuantityPerUnit = "", UnitPrice = 1.50,UnitsInStock = 2,UnitsOnOrder = 1, ReorderLevel = 5}).Wait();
+
+        }
+
+        [Test]
+        public void GivenProducts_GetAllProductsAsync_ReturnsAllProducts()
+        {
+            var result = _sut.GetAllProductsAsync();
+            Assert.That(result.Value(), Is.TypeOf<List<Product>>());
+        }
+
+        [Test]
+        public void GivenProduct_ AddProductAsync_ReturnsNewProduct()
+        {
+
+            var result = _sut.AddProductAsync(new Product { });
+            Assert.That(result.UnitPrice, Is.EqualTo(10))
+        }
+
+    }
+}
