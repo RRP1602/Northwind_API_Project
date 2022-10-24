c4b4daa4348a601747c109547b9ad86f077c4686
Lewis Kellett <lewisk1996@gmail.com>
2022-10-24 12:19:14 +0100

    Added testing project

---
+++ NorthwindAPI/NorthwindAPI_Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace NorthwindAPI_Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
