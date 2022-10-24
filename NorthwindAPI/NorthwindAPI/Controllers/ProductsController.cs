44c03badf1a02497f8009b39a5d43f8c72998074
Cesar Mello <cesarpcj@hotmail.com>
2022-10-24 14:02:55 +0100

    implemented controller

--- NorthwindAPI/NorthwindAPI/Controllers/ProductsController.cs
+++ NorthwindAPI/NorthwindAPI/Controllers/ProductsController.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Humanizer;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis;
 using Microsoft.EntityFrameworkCore;
 using NorthwindAPI.Models;
+using NorthwindAPI.Models.DTO;
 using NorthwindAPI.Services;
 
 namespace NorthwindAPI.Controllers
@@ -23,40 +26,80 @@ namespace NorthwindAPI.Controllers
 
         // GET: api/Products
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        public async Task<ActionResult<IEnumerable<DTOProduct>>> GetProducts()
         {
-            return await _context.Products.ToListAsync();
+            var products = await _service.GetAllProductsAsync();
+            var dto = products.Select(p => Utils.ProductToDto(p)).ToList();
+            return dto;
         }
 
         // GET: api/Products/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<Product>> GetProduct(int id)
+        public async Task<ActionResult<DTOProduct>> GetProduct(int id)
         {
-            var product = await _context.Products.FindAsync(id);
+            var product = await _service.GetProductByIdAsync(id);
 
             if (product == null)
             {
                 return NotFound();
             }
 
-            return product;
+            return Utils.ProductToDto(product);
+        }
+
+        // GET: api/Products/ByName/phone
+        [HttpGet("/ByName/{name}")]
+        public async Task<ActionResult<DTOProduct>> GetProductByName(string name)
+        {
+            var product = await _service.GetProductByNameAsync(name);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Utils.ProductToDto(product);
+        }
+
+        // GET: api/Products/ByCategory/1
+        [HttpGet("/ByCategory/{category}")]
+        public async Task<ActionResult<IEnumerable<DTOProduct>>> GetProductByCategoryId(int categoryId)
+        {
+            var product = await _service.GetProductByCategoryIdAsync(categoryId);
+
+            var dto = product.Select(p => Utils.ProductToDto(p)).ToList();           
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return dto;
         }
 
         // PUT: api/Products/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutProduct(int id, Product product)
+        public async Task<IActionResult> PutProduct(int id, DTOProduct dto)
         {
-            if (id != product.ProductId)
+            if (id != dto.ProductId)
             {
                 return BadRequest();
             }
 
-            _context.Entry(product).State = EntityState.Modified;
+            var product = await _service.GetProductByIdAsync(id);
+
+
+            product.CategoryId = dto.CategoryId ?? product.CategoryId;
+            product.ProductName = dto.ProductName ?? product.ProductName;
+            product.SupplierId = dto.SupplierId ?? product.SupplierId;
+            product.UnitPrice = dto.UnitPrice ?? product.UnitPrice;
+
+            //_service.Entry(product).State = EntityState.Modified;
 
             try
             {
-                await _context.SaveChangesAsync();
+                await _service.SaveChangesAsync();
             }
             catch (DbUpdateConcurrencyException)
             {
@@ -76,10 +119,9 @@ namespace NorthwindAPI.Controllers
         // POST: api/Products
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public async Task<ActionResult<Product>> PostProduct(Product product)
+        public async Task<ActionResult<Product>> PostProduct(DTOProduct product)
         {
-            _context.Products.Add(product);
-            await _context.SaveChangesAsync();
+            await _service.AddProductAsync(Utils.DtoToProduct(product));
 
             return CreatedAtAction("GetProduct", new { id = product.ProductId }, product);
         }
@@ -88,21 +130,22 @@ namespace NorthwindAPI.Controllers
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteProduct(int id)
         {
-            var product = await _context.Products.FindAsync(id);
+            var product = await _service.GetProductByIdAsync(id);
             if (product == null)
             {
                 return NotFound();
             }
 
-            _context.Products.Remove(product);
-            await _context.SaveChangesAsync();
+            await _service.RemoveProductAsync(product);
 
             return NoContent();
         }
 
         private bool ProductExists(int id)
         {
-            return _context.Products.Any(e => e.ProductId == id);
+
+            //return _service.Products.Any(e => e.ProductId == id);
+            return _service.ProductsExsits(id);
         }
     }
 }
