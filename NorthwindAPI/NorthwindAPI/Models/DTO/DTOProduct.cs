c23556a224a9c9a6a660023834f6bf5acb923814
Lewis Kellett <lewisk1996@gmail.com>
2022-10-24 12:02:25 +0100

    Added DTO's

---
+++ NorthwindAPI/NorthwindAPI/Models/DTO/DTOProduct.cs
@@ -0,0 +1,11 @@
+﻿namespace NorthwindAPI.Models.DTO
+{
+    public class DTOProduct
+    {
+        public int ProductId { get; set; }
+        public string? ProductName { get; set; }
+        public int? SupplierId { get; set; }
+        public int? CategoryId { get; set; }
+        public decimal? UnitPrice { get; set; }
+    }
+}
